651716b9623fc61a8912b2aec6ef1a3715e62b0e
VincentVanBork <michal.darowny@gmail.com>
2023-01-29 23:22:33 +0100

    day2 second part

--- day2/main.go
+++ day2/main.go
@@ -10,7 +10,52 @@ import (
 var options = map[string]int{"A": 0, "B": 1, "C": 2, "X": 0, "Y": 1, "Z": 2}
 
 func getOptions(opp string, player string) (int, int) {
-	return options[opp], options[player]
+	// X loose y draw z win
+	// spaghetti code here we go
+	option := 0
+	switch player {
+	case "X":
+		switch opp {
+		case "A":
+			option = 2
+			break
+		case "B":
+			option = 0
+			break
+		case "C":
+			option = 1
+			break
+		}
+		break
+	case "Y":
+		switch opp {
+		case "A":
+			option = 0
+			break
+		case "B":
+			option = 1
+			break
+		case "C":
+			option = 2
+			break
+		}
+		break
+	case "Z":
+		switch opp {
+		case "A":
+			option = 1
+			break
+		case "B":
+			option = 2
+			break
+		case "C":
+			option = 0
+			break
+		}
+		break
+	}
+	// row and column
+	return option, options[opp]
 }
 
 func getRules() [][]int {
@@ -32,7 +77,7 @@ func play(opponent string, player string) int {
 	rulesMatrix := getRules()
 	// fmt.Println(rulesMatrix)
 	i, j := getOptions(opponent, player)
-	return rulesMatrix[j][i]
+	return rulesMatrix[i][j]
 }
 func main() {
 	if !input.CheckExists(2) {
